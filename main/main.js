f3fad9fda6e3b84bdcea9d5d57db9ad3e07476fd
zhoujs1995 <13212289242@qq.com>
2019-05-26 19:58:51 +0800

    编写main

--- main/main.js
+++ main/main.js
@@ -1,4 +1,39 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+	var str_1 = "", str_2 = "", str_3 = "";
+
+	for(var i = 0; i < str.length; i++) {
+
+		if(str[i] == "0"){
+			str_1 = str_1 + "._."; str_2 = str_2 + "|.|"; str_3 = str_3 + "|_|";
+		}else if(str[i] == "1") {
+			str_1 = str_1 + "..."; str_2 = str_2 + "..|"; str_3 = str_3 + "..|";
+		}else if(str[i] == "2") {
+			str_1 = str_1 + "._."; str_2 = str_2 + "._|"; str_3 = str_3 + "|_.";
+		}else if(str[i] == "3") {
+			str_1 = str_1 + "._."; str_2 = str_2 + "._|"; str_3	= str_3 + "._|";	
+		}else if(str[i] == "4") {
+			str_1 = str_1 + "..."; str_2 = str_2 + "|_|"; str_3 = str_3 + "..|";
+		}else if(str[i] == "5") {
+			str_1 = str_1 + "._."; str_2 = str_2 + "|_."; str_3 = str_3 + "._|";
+		}else if(str[i] == "6") {
+			str_1 = str_1 + "._."; str_2 = str_2 + "|_."; str_3 = str_3 + "|_|";
+		}else if(str[i] == "7") {
+			str_1 = str_1 + "._."; str_2 = str_2 + "..|"; str_3 = str_3 + "..|";
+		}else if(str[i] == "8") {
+			str_1 = str_1 + "._."; str_2 = str_2 + "|_|"; str_3 = str_3 + "|_|";
+		}else if(str[i] == "9") {
+			str_1 = str_1 + "._."; str_2 = str_2 + "|_|"; str_3 = str_3 + "..|";
+		}else{
+			break;
+		}
+
+		if(i != str.length - 1) {
+			str_1 = str_1 + " "; str_2 = str_2 +" "; str_3 = str_3 +" ";	
+		}
+	}	
+
+
+    var strs = str_1 + "\n" + str_2 + "\n" + str_3 + "\n";
+    console.log(strs)
+	return strs;
 };
